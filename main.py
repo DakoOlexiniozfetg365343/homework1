9348c44666745dc9f6faa59693e46bc5853f88d1
Олександр Дако <129578271+DakoOlexiniozfetg365343@users.noreply.github.com>
2023-04-01 15:00:04 +0000

    added def

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+
+class Cat:
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.hunger = 50
+        self.energy = 50
+        self.mood = 50
+
+
+  
