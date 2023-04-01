b4c10d5dddbfd37cc70e6af227732ba4f8f8ea9b
Олександр Дако <129578271+DakoOlexiniozfetg365343@users.noreply.github.com>
2023-04-01 15:01:09 +0000

    added 2 another def

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ class Cat:
         self.hunger = 50
         self.energy = 50
         self.mood = 50
+      
+    def eat(self):
+        self.hunger -= 10
+        self.energy += 10
+        self.mood += 5
+        print(self.name + " :Я затоптав борща з салом.")
 
+    def sleep(self):
+        self.energy += 20
+        self.hunger += 5
+        self.mood += 10
+        print(self.name + " :Я добряче поспав.")
 
   
